3fe9cc6db2fd973d014e60f9ad1e184c4d4fb20c
ShaneEdw7 <130687464+ShaneEdw7@users.noreply.github.com>
2023-05-01 16:24:33 -0230

    Added automatic popup to start new games.

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        1250
+    )
+});
