a8bc67b32ae1ebd115179a6ee9aad5462ddceba2
ShaneEdw7 <130687464+ShaneEdw7@users.noreply.github.com>
2023-05-03 21:25:22 -0230

    Added Modal Popup & Nav Bar to New Game
    Work in progress

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", function(){
-    setTimeout(
-        function open(event){
-            document.querySelector(".popup").style.display = "block";
-        },
-        1250
-    )
-});
