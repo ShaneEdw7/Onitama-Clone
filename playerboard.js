f3b350a62cab387025d6802012506d94145489cb
ShaneEdw7 <130687464+ShaneEdw7@users.noreply.github.com>
2023-05-19 16:45:14 -0230

    Created Game Board - Minor Styling Updates

--- playerboard.js
+++ playerboard.js
@@ -1,8 +1,25 @@
-const gamemat = document.getElementById("gamemat");
-const canvas = gamemat.getContext("2d");
+// Selections from index
+const playercolor = localStorage.getItem('playercolor');
+const difficulty = localStorage.getItem('difficulty');
+// Canvas (Gameboard)
+const gameboard = document.getElementById("gameboard");
+const ctx = gameboard.getContext("2d");
 
-function drawBoard() {
-  canvas.fillStyle = "brown";
-  canvas.fillRect(0, 0, gamemat.width, gamemat.height);
+const boardSize = gameboard.width;
+const numCells = 5;
+const cellSize = boardSize / numCells;
+
+function drawGameboard(){
+ctx.beginPath();
+for (let x = 0; x <= boardSize; x += cellSize) {
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, boardSize);
+    }
+for (let y = 0; y <= boardSize; y += cellSize) {
+    ctx.moveTo(0, y);
+    ctx.lineTo(boardSize, y);
+    }
+ctx.strokeStyle = "894E24";
+ctx.stroke();
 }
-drawBoard();
+drawGameboard()
--- script.js
+++ script.js
@@ -2,17 +2,15 @@ let startbtn = document.getElementById('start');
 let playercolor = document.getElementById('playercolor');
 let aidiff = document.getElementById('aidiff')
 
-    startbtn.addEventListener('click', () => {
-        let colordisplay = document.getElementsByName('color');
-        colordisplay.forEach((color) => {
-           if (color.checked) {
-              playercolor.innerText = `Player Color: ${color.value}`;
-              }
-         });
-         let difflevel = document.getElementsByName('difficulty');
-         difflevel.forEach((difficulty) => {
-            if (difficulty.checked) {
-               aidiff.innerText = `Difficulty: ${difficulty.value}`;
-               }
-          });
-    }); 
+const colordisplay = document.getElementsByName('color');
+colordisplay.forEach((color) => {
+    color.addEventListener('click', (event) => {
+        localStorage.setItem('playercolor', event.target.value)
+    })
+});
+const difflevel = document.getElementsByName('difficulty');
+difflevel.forEach((difficulty) => {
+    difficulty.addEventListener('click', (event) => {
+        localStorage.setItem('difficulty', event.target.value)
+    })
+});
