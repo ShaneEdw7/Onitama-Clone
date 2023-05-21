c79184071e377c705226f12b7f5aaea0d057b485
ShaneEdw7 <130687464+ShaneEdw7@users.noreply.github.com>
2023-05-21 09:56:50 -0230

    Added test pawn

--- playerboard.js
+++ playerboard.js
@@ -23,3 +23,70 @@ ctx.strokeStyle = "894E24";
 ctx.stroke();
 }
 drawGameboard()
+
+// Game Pieces
+const pawn = new Image()
+pawn.src = 'pawn.png';
+
+let pawnx = 145;
+let pawny = 295;
+const speed = 2;
+
+pawn.onload = function() {
+    let newWidth = 110;
+    let newHeight = 90; 
+    ctx.drawImage(pawn, 145, 295, newWidth, newHeight);
+    gameboard.addEventListener('mousedown', handleMouseDown);
+    gameboard.addEventListener('mousemove', handleMouseMove);
+    gameboard.addEventListener('mouseup', handleMouseUp);
+};
+
+let isDragging = false;
+let startX, startY;
+
+
+function handleMouseDown(event) {
+  const rect = gameboard.getBoundingClientRect();
+  startX = event.clientX - rect.left;
+  startY = event.clientY - rect.top;
+
+  if (
+    startX >= pawnx &&
+    startX <= pawnx + pawn.width &&
+    startY >= pawny &&
+    startY <= pawny + pawn.height
+  ) {
+    isDragging = true;
+  }
+}
+
+function handleMouseMove(event) {
+  if (isDragging) {
+    const rect = gameboard.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+
+    const dx = mouseX - startX;
+    const dy = mouseY - startY;
+
+    pawnx += dx;
+    pawny += dy;
+
+    startX = mouseX;
+    startY = mouseY;
+  }
+}
+
+function handleMouseUp() {
+  isDragging = false;
+}
+
+function animate() {
+    newWidth = 110;
+    newHeight = 90;
+    ctx.clearRect(0, 0, gameboard.width, gameboard.height);
+    ctx.drawImage(pawn, pawnx, pawny, newHeight, newWidth);
+    drawGameboard() 
+    requestAnimationFrame(animate);
+}
+animate();
